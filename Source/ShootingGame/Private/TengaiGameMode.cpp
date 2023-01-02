4658c319e267ae4b8936a5a9430fce277f39d364
TGLEEEE <121008880+TGLEEEE@users.noreply.github.com>
2023-01-02 16:26:43 +0900

    alpha pakaging

--- Source/ShootingGame/Private/TengaiGameMode.cpp
+++ Source/ShootingGame/Private/TengaiGameMode.cpp
@@ -2,8 +2,10 @@
 
 
 #include "TengaiGameMode.h"
+#include "GameFramework/GameUserSettings.h"
+#include "Engine.h"
 
 void ATengaiGameMode::BeginPlay()
 {
-
+	GEngine->GetGameUserSettings()->SetScreenResolution(FIntPoint(1920, 1080));
 }
