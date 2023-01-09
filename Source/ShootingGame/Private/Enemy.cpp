32b5b846dcc64ff011b6c0aa841d8ebd077e843d
TGLEEEE <121008880+TGLEEEE@users.noreply.github.com>
2023-01-09 22:13:07 +0900

    fix: 적 아이템스폰 복구

--- Source/ShootingGame/Private/Enemy.cpp
+++ Source/ShootingGame/Private/Enemy.cpp
@@ -191,12 +191,12 @@ void AEnemy::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherAc
 
 			if (drawRate <= dropRate)
 			{
-				GetWorld()->SpawnActor<AItem>(itemFactory, GetActorLocation() + FVector(0, 0, -100), FRotator::ZeroRotator);
+				GetWorld()->SpawnActor<AItem>(itemFactory, GetActorLocation() + FVector(0, 0, -100), FRotator(0, 90, 0));
 			}
 
 			if (drawRateUlti <= dropRateUlti)
 			{
-				GetWorld()->SpawnActor<AItem>(itemFactoryUlti, GetActorLocation() + FVector(0, 0, -100), FRotator::ZeroRotator);
+				GetWorld()->SpawnActor<AItem>(itemFactoryUlti, GetActorLocation() + FVector(0, 0, -100), FRotator(0, 90, 0));
 			}
 			DestroyEnemy();
 		}
