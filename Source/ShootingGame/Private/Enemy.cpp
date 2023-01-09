b60435e3056a7880d50fc771d5a2c3921c795bfa
TGLEEEE <121008880+TGLEEEE@users.noreply.github.com>
2023-01-09 13:02:21 +0900

    working

--- Source/ShootingGame/Private/Enemy.cpp
+++ Source/ShootingGame/Private/Enemy.cpp
@@ -192,13 +192,13 @@ void AEnemy::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherAc
 			if (drawRate <= dropRate)
 			{
 				GetWorld()->SpawnActor<AItem>(itemFactory, GetActorLocation() + FVector(0, 0, -100), FRotator(0, 90, 0));
-				UE_LOG(LogTemp, Warning, TEXT("power item spawned"));
+				//UE_LOG(LogTemp, Warning, TEXT("power item spawned"));
 			}
 
 			if (drawRateUlti <= dropRateUlti)
 			{
 				GetWorld()->SpawnActor<AItem>(itemFactoryUlti, GetActorLocation() + FVector(0, 0, -100), FRotator(0, 90, 0));
-				UE_LOG(LogTemp, Warning, TEXT("ult item spawned"));
+				//UE_LOG(LogTemp, Warning, TEXT("ult item spawned"));
 			}
 			DestroyEnemy();
 		}
